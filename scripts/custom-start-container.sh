07d30b2f325fa8da05aa999aee37e19beff11e18
Kien Ngo <ngtkien95@gmail.com>
2025-11-15 22:55:56 +0700

    Update custom-start-container.sh

--- scripts/custom-start-container.sh
+++ scripts/custom-start-container.sh
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-./sources/custom-host-docker-containers/run.sh -u 22.04 -w "$WORKSPACE_DIR" -p
+./sources/custom-host-docker-containers/run.sh -u 22.04 -w "$WORKSPACE_DIR" -l -p
 
 if [ $? -ne 0 ]; then
     echo "#################################################"
